fa8381513957972b6789e2b42ea642df7660ae7f
SamuelLangenfeld <samlangenfeld@gmail.com>
2017-03-27 08:39:03 -0400

    added users controller to check how things work, will need to remove it again

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
   post '/signin', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  get '/user', to: 'users#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
